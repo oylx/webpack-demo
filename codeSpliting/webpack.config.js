e44dfd765a2973155852cce5c8458509fd9cd371
oylx <oylx2008@163.com>
2019-11-05 14:47:59 +0800

    修改codeSplitting

--- codeSpliting/webpack.config.js
+++ codeSpliting/webpack.config.js
@@ -4,10 +4,11 @@ const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 
 module.exports = {
   entry: {
-    app: './src/app.js' // 需要打包的文件入口
+    main: './src/index.js'
   },
   output: {
-    publicPath: __dirname + '/dist/', // js 引用的路径或者 CDN 地址
+    publicPath: './dist/', // js 引用的路径或者 CDN 地址
+    // publicPath: __dirname + '/dist/', // js 引用的路径或者 CDN 地址
     path: path.resolve(__dirname, 'dist'), // 打包文件的输出目录
     filename: '[name].bundle.js', // 代码打包后的文件名
     chunkFilename: '[name].js' // 代码拆分后的文件名
@@ -17,12 +18,24 @@ module.exports = {
       chunks: 'async',
       minSize: 30000,
       maxSize: 0,
-      minChunks: 1,
+      minChunks: 2,
       maxAsyncRequests: 5,
       maxInitialRequests: 3,
       automaticNameDelimiter: '~',
       name: true,
       cacheGroups: {
+        lodash: {
+          name: 'lodash',
+          test: /[\\/]node_modules[\\/]lodash[\\/]/,
+          priority: 10
+        },
+        commons: {
+          name: 'commons',
+          minSize: 0, //表示在压缩前的最小模块大小,默认值是 30kb
+          minChunks: 2, // 最小公用次数
+          priority: 5, // 优先级
+          reuseExistingChunk: true // 公共模块必开启
+        },
         vendors: {
           test: /[\\/]node_modules[\\/]/,
           priority: -10
@@ -35,6 +48,7 @@ module.exports = {
       }
     }
   },
-
-  plugins: [new CleanWebpackPlugin()]
+  plugins: [
+    new CleanWebpackPlugin() // 默认情况下，此插件将删除 webpack output.path 目录中的所有文件，以及每次成功重建后所有未使用的 webpack 资产。
+  ]
 }
